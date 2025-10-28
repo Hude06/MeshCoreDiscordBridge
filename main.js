4eb12d9020e6ef696a4fd69953f0cdccf3461463
Hude06 <judeallanhill@icloud.com>
2025-10-27 22:06:37 -0700

    huh?

--- main.js
+++ main.js
@@ -47,6 +47,7 @@ connection.on(Constants.PushCodes.LogRxData, async (event) => {
     const contactPrefix = hex.slice(0, 2);
     // console.log(json.path,contactPrefix,hex,json)
     for (let i = 0; i < json.path.length; i++) {
+      console.log("PATH IS ",json.path[i])
         console.log("prefix is",bytesToHex(json.path[i]));
       if ((bytesToHex(json.path[i])).toString() === contactPrefix) {
         console.log("Matched contact:", contact.advName);
