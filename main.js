0d817d0cb33c31b06ea5a260917c012d7daa6b2a
Hude06 <judeallanhill@icloud.com>
2025-10-28 17:23:46 -0700

    adding more debug so much debug

--- main.js
+++ main.js
@@ -39,6 +39,7 @@ connection.on(Constants.PushCodes.LogRxData, async (event) => {
 
     // console.log(json.path,contactPrefix,hex,json)
   const path = [];
+  console.log(json.payload_type_string)
   for (let i = 0; i < json.path.length; i++) {
     // const byte = parseInt(json.path[i], 16);
     console.log(json.path[i]);
