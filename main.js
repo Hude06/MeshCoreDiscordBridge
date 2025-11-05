7796d3f132854edca1b0a86c2137ffe2e0ff9d05
Hude06 <judeallanhill@icloud.com>
2025-11-05 15:09:46 -0800

    tets

--- main.js
+++ main.js
@@ -32,8 +32,8 @@ connection.on(Constants.PushCodes.LogRxData, async (event) => {
     const pathBytes = Array.from(json.path); // stable copy
     let prefix = [];
     for (let i = 0; i < pathBytes.length; i++) {
-      prefix.push(pathBytes[i]); // accumulate full prefix up to this hop
-      console.log("Current PREFIX:", bytesToHex(Uint8Array.from(prefix)));
+      prefix.push(bytesToHex(Uint8Array.from(pathBytes[i]))); // accumulate full prefix up to this hop
+      console.log("Current PREFIX:", bytesToHex(Uint8Array.from(pathBytes[i])));
     }
     console.log("FINAL PATH:", prefix);
     if (discordChannel) {
